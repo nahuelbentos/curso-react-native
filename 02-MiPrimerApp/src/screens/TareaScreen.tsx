4432e7b0865bf069ebf33f1e784951da3b103c40
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2022-05-28 17:44:51 -0300

    Remove width in cajaNaranja

--- 02-MiPrimerApp/src/screens/TareaScreen.tsx
+++ 02-MiPrimerApp/src/screens/TareaScreen.tsx
@@ -20,22 +20,19 @@ const styles = StyleSheet.create({
   },
   cajaMorada: {
     width: 100,
-    height: '100%',
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#5856D6',
   },
   cajaNaranja: {
     width: 100,
-    height: '100%',
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#F0A23B',
   },
   cajaAzul: {
     width: 100,
-    height: '100%',
-    borderWidth: 10,
+    height: '100%',    borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#28C4D9',
   },
